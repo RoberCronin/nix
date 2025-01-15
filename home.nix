c24dbcdf99505fdd6d3287c7bee33a2f2b5e7a52
Robert Cronin <robert.t.cronin@outlook.com>
2025-01-15 12:45:21 -0800

    added neovim config

--- home.nix
+++ home.nix
@@ -10,4 +10,10 @@
 
         stateVersion = "23.11";
     };
+
+    programs.neovim = {
+        enable = true;
+        extraLuaPackages = ps: [ ps.magick ];
+        extraPackages = with pkgs; [ imagemagick ];
+    };
 }
