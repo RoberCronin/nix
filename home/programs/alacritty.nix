21d51279c1adf14ba07feb8fe5bcc8259f8d75af
Robert Cronin <robert.t.cronin@outlook.com>
2025-10-31 19:33:31 -0700

    stylix

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -1,5 +1,10 @@
 {...}: {
     programs.alacritty = {
         enable = true;
+        settings = {
+            general.import = [
+                "~/.config/alacritty/differences.toml"
+            ];
+        };
     };
 }
--- home/stylix.nix
+++ home/stylix.nix
@@ -2,9 +2,11 @@
     stylix = {
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-        #autoEnable = false;
-        #        targets = {
-        #            alacritty.enable = true;
-        #        };
+        fonts = {
+            monospace = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+        };
     };
 }
