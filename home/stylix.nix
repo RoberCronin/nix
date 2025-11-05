789e9b18fcb56d0f6e8639112780338b39206340
Robert Cronin <robert.t.cronin@outlook.com>
2025-11-04 18:48:54 -0800

    e

--- home/stylix.nix
+++ home/stylix.nix
@@ -1,7 +1,8 @@
 {pkgs, ...}: {
     stylix = {
         enable = true;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+        #base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
         fonts = {
             monospace = {
                 package = pkgs.nerd-fonts.jetbrains-mono;
