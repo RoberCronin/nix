e2b998c93d842457753a3ed68cbbe84402cec222
Robert Cronin <robert.t.cronin@outlook.com>
2025-11-23 23:56:14 -0800

    graphics drivers for tablet

--- hosts/tablet.nix
+++ hosts/tablet.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
     imports = [
         ../modules/nixos/wacom.nix
         ../modules/nixos/hardware/power-key.nix
@@ -8,4 +8,24 @@
     services.udev.extraRules = ''
         ACTION=="add", SUBSYSTEM=="serio", DRIVERS=="atkbd", ATTR{firmware_id}=="PNP: PNP0303", ATTR{id/extra}=="00", ATTR{id/id}=="00", ATTR{id/proto}=="00", ATTR{id/type}=="06", ATTR{power/wakeup}="disabled"
     '';
+
+    environment.sessionVariables = {
+        LIBVA_DRIVER_NAME = "iHD";
+    };
+
+    services.xserver.videoDrivers = [
+        "i915"
+        "intel"
+    ];
+
+    hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+            intel-media-driver
+            intel-vaapi-driver
+            libva-vdpau-driver
+            libvdpau-va-gl
+        ];
+    };
 }
