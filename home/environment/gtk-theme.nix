eac4d2857b03562ecb4bb31abfc942b6416d80f0
Robert Cronin <robert.t.cronin@outlook.com>
2025-12-21 18:50:04 -0800

    set gtk theme

--- home/environment/gtk-theme.nix
+++ home/environment/gtk-theme.nix
@@ -1,19 +1,16 @@
 {pkgs, ...}: {
-    nixpkgs.config.packageOverrides = pkgs: {
-        catppuccin-gtk = pkgs.catppuccin-gtk.override {
-            accents = ["mauve"]; # You can specify multiple accents here to outpu>
-            size = "compact";
-            tweaks = ["normal"]; # You can also specify multiple tweaks here
-            variant = "macchiato";
-        };
-    };
-
     gtk = {
         enable = true;
         iconTheme.package = pkgs.papirus-icon-theme;
         iconTheme.name = "Papirus-Dark";
-        #theme.package = pkgs.catppuccin-gtk;
-        #theme.name = "catppuccin-macchiato-mauve-compact+normal";
+        theme.package = pkgs.catppuccin-gtk.override {
+            accents = ["maroon"];
+            size = "compact";
+            tweaks = ["rimless"];
+            variant = "macchiato";
+        };
+
+        theme.name = "catppuccin-macchiato-maroon-compact+rimless";
         cursorTheme.package = pkgs.bibata-cursors;
         cursorTheme.name = "Bibata-Modern-Classic";
         cursorTheme.size = 24;
