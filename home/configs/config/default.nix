752a0eeb782486a403f100dbdd86f06e9230203c
Robert Cronin <robert.t.cronin@outlook.com>
2025-12-31 15:49:48 -0800

    fix config file symlinks

--- home/configs/config/default.nix
+++ home/configs/config/default.nix
@@ -1,63 +1,63 @@
 {config, ...}: {
     xdg.configFile = {
         "hypr" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./hypr;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/hypr";
             recursive = true;
         };
 
         # This file contains the differences between hosts.
         # It is imported in ~/.config/hypr/hyprland.conf
         "hyprland_host.conf" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./hypr/hosts/${config.host}.conf;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/hypr/hosts/${config.host}.conf";
         };
 
         "alacritty" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./alacritty;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/alacritty";
             recursive = true;
         };
 
         "better-control" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./better-control;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/better-control";
             recursive = true;
         };
 
         "nvim" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./nvim;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/nvim";
             recursive = true;
         };
 
         "waybar" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./waybar;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/waybar";
             recursive = true;
         };
 
         "btop" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./btop;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/btop";
             recursive = true;
         };
 
         "wofi" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./wofi;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/wofi";
             recursive = true;
         };
 
         "rofi" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./rofi;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/rofi";
             recursive = true;
         };
 
         "git" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./git;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/git";
             recursive = true;
         };
 
         "dunst" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./dunst;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/dunst";
             recursive = true;
         };
 
         "starship.toml" = {
-            source = config.lib.file.mkOutOfStoreSymlink ./starship.toml;
+            source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/home/configs/config/starship.toml";
         };
     };
 }
