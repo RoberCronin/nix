5e1e053cd89036857093d8ffdd600f2588f5a16f
Robert Cronin <robert.t.cronin@outlook.com>
2026-01-01 17:37:38 -0800

    fixed warning

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -47,7 +47,8 @@
 
             "NixOS Wiki" = {
                 urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
+                # iconUpdateURL = "https://nixos.wiki/favicon.png";
+                icon = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
                 definedAliases = ["@nw"];
             };
