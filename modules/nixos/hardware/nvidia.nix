963ee5746cfe0fd587da754a24b0ca8ed4e6265f
Robert Cronin <robert.t.cronin@outlook.com>
2026-01-04 21:30:03 -0800

    [nixos] change nvidia module

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -1,7 +1,16 @@
-{config, ...}: {
+{
+    config,
+    pkgs,
+    ...
+}: {
+    hardware.graphics = {
+        enable = true;
+    };
+
     environment.variables = {
         __GL_SYNC_DISPLAY_DEVICE = "DP-0";
     };
+
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia = {
         # Modesetting is required.
@@ -10,10 +19,22 @@
         powerManagement.finegrained = false;
 
         # Use the Nvidia open source kernel module (not nouveu)
-        open = false;
+        open = true;
         nvidiaSettings = true;
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
-    # nixpkgs.config.cudaSupport = true;
+    environment.systemPackages = with pkgs; [
+        libva-utils
+        vdpauinfo
+        vulkan-tools
+        vulkan-validation-layers
+        libvdpau-va-gl
+        egl-wayland
+        wgpu-utils
+        mesa
+        libglvnd
+        nvitop
+        libGL
+    ];
 }
