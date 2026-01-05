88d8528e97576a46656db23ed089a4dcce0e1cdb
Robert Cronin <robert.t.cronin@outlook.com>
2026-01-04 16:00:54 -0800

    [home-manager] backup and restore vscode extensions

--- home/config_files/.config/Code/default.nix
+++ home/config_files/.config/Code/default.nix
@@ -1,10 +1,23 @@
-{config, ...}: {
+{
+    config,
+    lib,
+    pkgs,
+    ...
+}: {
+    home.activation.vscodeRestoreExtensions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        cd "$XDG_CONFIG_HOME/Code/User" &&
+        cat vscode-extensions.txt | xargs -n 1 ${pkgs.vscode}/bin/code --install-extension
+    '';
+
     xdg.configFile = {
         "Code/User/keybindings.json" = {
             source = config.lib.meta.mkMutableSymlink ./User/keybindings.json;
         };
         "Code/User/settings.json" = {
             source = config.lib.meta.mkMutableSymlink ./User/settings.json;
+        };
+        "Code/User/vscode-extensions.txt" = {
+            source = config.lib.meta.mkMutableSymlink ./User/vscode-extensions.txt;
         };
     };
 }
---
+++ scripts/backup_vscode_extensions.sh
@@ -0,0 +1,3 @@
+cd "$XDG_CONFIG_HOME/Code/User"
+code --list-extensions > vscode-extensions.txt
+
