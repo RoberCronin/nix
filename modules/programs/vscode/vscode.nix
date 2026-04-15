5f4265259c34b06a720daa0dd927710b8c1d6b93
Robert Cronin <robert.t.cronin@outlook.com>
2026-04-15 12:47:11 -0700

    moved vscode config

---
+++ modules/programs/vscode/vscode.nix
@@ -0,0 +1,47 @@
+{
+    flake.modules.nixos.vscode = {
+        inputs,
+        pkgs,
+        ...
+    }: {
+        home-manager.sharedModules = [inputs.self.modules.homeManager.vscode];
+        environment.systemPackages = with pkgs; [
+            vscode
+        ];
+    };
+
+    flake.modules.homeManager.vscode = {
+        lib,
+        config,
+        pkgs,
+        ...
+    }: {
+        home.activation.vscodeRestoreExtensions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+            cd "${config.home.homeDirectory}/.config/Code/User"
+
+            package_list=$(cat vscode-extensions.txt)
+            install_args=()
+            for arg in $package_list; do
+                install_args+=" --install-extension "
+                install_args+=$arg
+            done
+
+            ${pkgs.vscode}/bin/code $install_args
+        '';
+
+        xdg.configFile = {
+            "Code/User/keybindings.json" = {
+                source = config.lib.meta.mkMutableSymlink ./_config/User/keybindings.json;
+            };
+            "Code/User/settings.json" = {
+                source = config.lib.meta.mkMutableSymlink ./_config/User/settings.json;
+            };
+            "Code/User/tasks.json" = {
+                source = config.lib.meta.mkMutableSymlink ./_config/User/tasks.json;
+            };
+            "Code/User/vscode-extensions.txt" = {
+                source = config.lib.meta.mkMutableSymlink ./_config/User/vscode-extensions.txt;
+            };
+        };
+    };
+}
