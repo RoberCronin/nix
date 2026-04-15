db07dbec5d4d2abb9d367d7673ba549abfdd9dbf
Robert Cronin <robert.t.cronin@outlook.com>
2026-04-15 12:53:36 -0700

    moved fcitx5 config
    removed unnessesary package (fcitx5)

---
+++ modules/programs/fcitx5/fcitx5.nix
@@ -0,0 +1,22 @@
+{
+    flake.modules.nixos.fcitx5 = {
+        inputs,
+        pkgs,
+        ...
+    }: {
+        home-manager.sharedModules = [inputs.self.modules.homeManager.fcitx5];
+
+        i18n.inputMethod.enable = true;
+        i18n.inputMethod.type = "fcitx5";
+        i18n.inputMethod.fcitx5.addons = with pkgs; [fcitx5-mozc];
+    };
+
+    flake.modules.homeManager.fcitx5 = {config, ...}: {
+        home.file = {
+            ".local/share/fcitx5" = {
+                source = config.lib.meta.mkMutableSymlink ./_config;
+                recursive = true;
+            };
+        };
+    };
+}
