ed534dfc7b622be07432f7a391e5006e37fa2d9e
Robert Cronin <robert.t.cronin@outlook.com>
2026-04-15 13:03:53 -0700

    added user

---
+++ modules/users/robert.nix
@@ -0,0 +1,24 @@
+{
+    flake.modules.nixos.users.robert = {config, ...}: {
+        users.users.robert = {
+            isNormalUser = true;
+            name = "robert";
+            description = "Robert Cronin";
+            extraGroups = [
+                "uinput"
+                "fuse"
+                "docker"
+                "networkmanager"
+                "wheel"
+                "uucp"
+                "dialout"
+                "vboxusers"
+                "audio"
+            ];
+        };
+
+        environment.sessionVariables = {
+            NH_FLAKE = config.flakePath;
+        };
+    };
+}
