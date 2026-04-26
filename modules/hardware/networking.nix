dc60359eb5ed382fc86068db5edc436aef255981
Robert Cronin <robert.t.cronin@outlook.com>
2026-04-25 18:35:34 -0700

    add openvpn package to networkmanager

--- modules/hardware/networking.nix
+++ modules/hardware/networking.nix
@@ -1,10 +1,18 @@
 {
-    flake.modules.nixos.base = {config, ...}: {
-        networking.networkmanager.enable = true;
+    flake.modules.nixos.base = {
+        config,
+        pkgs,
+        ...
+    }: {
         networking.firewall.allowedTCPPorts = [80 443 1401 631];
         networking.hostName = config.host;
         networking.firewall.checkReversePath = "loose";
-        networking.wireguard.enable = true;
         networking.iproute2.enable = true;
+        networking.wireguard.enable = true;
+        programs.openvpn3.enable = true;
+        networking.networkmanager = {
+            enable = true;
+            packages = [pkgs.networkmanager-openvpn];
+        };
     };
 }
