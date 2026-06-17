369901b0d87ba4ca19cee3752df841a940583e7d
Robert Cronin <robert.t.cronin@outlook.com>
2026-06-16 18:43:49 -0700

    [nvim] add filetype to wordcount

--- modules/programs/nvim/_config/lua/plugins/prose.lua
+++ modules/programs/nvim/_config/lua/plugins/prose.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         require("nvim-prose").setup({
             wpm = 200.0,
-            filetypes = { "markdown", "asciidoc", "typst" },
+            filetypes = { "markdown", "asciidoc", "typst", "text" },
             placeholders = {
                 words = "words",
                 minutes = "min",
