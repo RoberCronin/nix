51156a303fd62128b0bea4385216aad3cdc61425
Robert Cronin <robert.t.cronin@outlook.com>
2026-08-04 22:06:40 -0700

    [nixos] (hopefully) fixed audio issues with headphones

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -2,11 +2,21 @@
     flake.modules.nixos.audio = {...}: {
         services.pulseaudio.enable = false;
         security.rtkit.enable = true;
+        boot.kernelParams = ["snd_usb_audio.power_save=0"];
         services.pipewire = {
             enable = true;
             alsa.enable = true;
             alsa.support32Bit = true;
             pulse.enable = true;
+
+            extraConfig.pipewire."99-buffer" = {
+                "context.properties" = {
+                    "default.clock.quantum" = 1024;
+                    "default.clock.min-quantum" = 1024;
+                    "default.clock.max-quantum" = 2048;
+                    "default.clock.rate" = 48000;
+                };
+            };
         };
     };
 }
--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -7,14 +7,29 @@
             settings = {
                 General = {
                     Name = config.host + "bluetooth";
-                    ControllerMode = "dual";
+                    ControllerMode = "bredr";
                     FastConnectable = "true";
                     Experimental = "true";
                 };
+
                 Policy = {
                     AutoEnable = "true";
                 };
             };
         };
+
+        services.pipewire.wireplumber.extraConfig."10-bluez" = {
+            "monitor.bluez.properties" = {
+                "bluez5.codecs" = ["sbc" "sbc_xq" "aac" "aptx" "aptx_hd"];
+            };
+        };
+
+        boot.extraModprobeConfig = ''
+            options btusb enable_autosuspend=n
+
+            options iwlwifi bt_coex_active=Y
+            options iwlwifi power_save=N
+            options iwlmvm power_scheme=1
+        '';
     };
 }
